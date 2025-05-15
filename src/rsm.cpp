a64b738ef6e6be3090f95615bd776bc13f7c9f5d
Lukáš Růžička (asus-haven-w) <ruzovej@gmail.com>
2025-05-15 08:33:01 +0200

    rsm: reformats one file

--- src/rsm.cpp
+++ src/rsm.cpp
@@ -8,7 +8,9 @@ namespace rsm {
 
 void init_thread_local_sink() { impl::thread_local_sink::init(); }
 
-void flush_thread() noexcept { impl::thread_local_sink::instance()->flush_to_global(); }
+void flush_thread() noexcept {
+  impl::thread_local_sink::instance()->flush_to_global();
+}
 
 void dump_collected_records(output::format const fmt,
                             char const *const filename) {
@@ -18,7 +20,8 @@ void dump_collected_records(output::format const fmt,
 
 void marker::append_record(long long const start_ns,
                            long long const end_ns) noexcept {
-  impl::thread_local_sink::instance()->append_record({desc, color, tag, start_ns, end_ns});
+  impl::thread_local_sink::instance()->append_record(
+      {desc, color, tag, start_ns, end_ns});
 }
 
 } // namespace rsm
