9b656929fa07b5796cde1ba9a62efa0a3decfaa3
Ruzovej <48952448+Ruzovej@users.noreply.github.com>
2025-06-08 00:43:09 +0200

    Better guard for `thread_local ... impl::local_sink` initialization (#63)

--- examples/instant/2.cpp
+++ examples/instant/2.cpp
@@ -19,18 +19,21 @@ int main(int argc, char const **argv) {
     // & ui.perfetto.dev display it somehow unusably ...
 
     std::thread t1{[]() {
+      CXXST_init_thread_local_sink();
       std::this_thread::sleep_for(std::chrono::milliseconds(1));
       CXXST_mark_complete("thread 1");
       CXXST_mark_instant("thread 1 started");
     }};
 
     std::thread t2{[]() {
+      CXXST_init_thread_local_sink();
       CXXST_mark_complete("thread 2");
       std::this_thread::sleep_for(std::chrono::milliseconds(1));
       CXXST_mark_instant("thread 2 started");
     }};
 
     std::thread t3{[]() {
+      CXXST_init_thread_local_sink();
       CXXST_mark_complete("thread 3");
       CXXST_mark_instant("thread 3 started");
       std::this_thread::sleep_for(std::chrono::milliseconds(1));
