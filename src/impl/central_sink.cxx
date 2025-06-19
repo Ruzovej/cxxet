ecc19f650c373f6271d46ec534e0c75fbc2663d3
Lukáš Růžička (asus-haven-w) <ruzovej@gmail.com>
2025-06-19 17:00:20 +0200

    Add copyright & license notice to all `C++` source code files

--- src/impl/central_sink.cxx
+++ src/impl/central_sink.cxx
@@ -1,3 +1,15 @@
+/*
+  Copyright 2025 Lukáš Růžička (ruzovej@gmail.com, https://github.com/Ruzovej/cxxet)
+
+  This file is part of cxxet.
+
+  cxxet is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+  cxxet is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License along with cxxet. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "impl/central_sink.hxx"
 
 #include <iostream>
