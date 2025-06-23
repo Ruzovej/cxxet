ccbca0b061c8e6a1844d34ea457ad7ca993ebbca
Lukáš Růžička (asus-haven-w) <ruzovej@gmail.com>
2025-06-23 08:43:24 +0200

    Add one small TODO

--- include/all/cxxet/all.hxx
+++ include/all/cxxet/all.hxx
@@ -63,6 +63,9 @@
 
 #define CXXET_sink_global_flush(...) cxxet::sink_global_flush(__VA_ARGS__)
 
+// TODO either rename this file (e.g. to `all_markers`) or provide (macro?!)
+// interface to `sink_diversion.hxx`
+
 #else
 
 #define CXXET_mark_complete(...)
