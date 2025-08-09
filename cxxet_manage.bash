c576c4afce4977e562f926809b7d9aa4e9650e74
Lukáš Růžička (asus-haven-w) <ruzovej@gmail.com>
2025-08-09 12:48:34 +0200

    Add "usage" description and better error handling to `cxxet_manage`

--- cxxet_manage.bash
+++ cxxet_manage.bash
@@ -17,6 +17,27 @@ set -e
     export CXXET_ROOT_DIR
 }
 
+COMMAND_NAME="$1"
+shift
+COMMAND_FILE="${CXXET_ROOT_DIR}/scripts/commands/${COMMAND_NAME}.bash"
+
+if [[ "${COMMAND_NAME}" == "--help" || "${COMMAND_NAME}" == "-h" || -z "${COMMAND_NAME}" || ! -f "${COMMAND_FILE}" ]]; then
+    RET_CODE=0
+    if [[ -z "${COMMAND_NAME}" ]]; then
+        printf 'Error: Command name is required!\n' >&2
+        RET_CODE=1
+    elif [[ "${COMMAND_NAME}" != "--help" && "${COMMAND_NAME}" != "-h" ]]; then
+        printf 'Error: Command "%s" not found!\n' "${COMMAND_NAME}" >&2
+        RET_CODE=1
+    fi
+    printf 'Usage: cxxet_manage <command> [command_specific_args...]\n' >&2
+    printf 'Available commands:\n' >&2
+    for POSSIBLE_COMMAND in "${CXXET_ROOT_DIR}/scripts/commands/"*.bash; do
+        printf ' - %s\n' "$(basename "${POSSIBLE_COMMAND}" .bash)" >&2
+    done
+    exit "${RET_CODE}"
+fi
+
 function cxxet_include() {
     local module_name="${1:?}"
     local module_path="${CXXET_ROOT_DIR:?}/${module_name}.bash"
@@ -37,8 +58,5 @@ function cxxet_include() {
     source "${module_path}"
 }
 export -f cxxet_include
-
-COMMAND_NAME="${1:?}"
-shift
 
 bash -c "cxxet_include scripts/commands/${COMMAND_NAME} && ${COMMAND_NAME} $*"
