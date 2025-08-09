a17be710174d9633e722cbfb288a3460bef4da69
Lukáš Růžička (asus-haven-w) <ruzovej@gmail.com>
2025-08-09 15:07:03 +0200

    Small `cxxet_require_command` refactoring

--- scripts/base/cxxet_require_command.bash
+++ scripts/base/cxxet_require_command.bash
@@ -3,12 +3,14 @@
 set -e
 
 function cxxet_require_command() {
-    local command_name="${1:?}"
-
-    if ! command -v "${command_name}" &> /dev/null; then
-        printf 'Error: "%s" is not installed or not in PATH\n' "${command_name}" >&2
-        exit 1
-    fi
+    while (( $# > 0 )); do
+        local command_name="$1"
+        if ! command -v "${command_name}" &> /dev/null; then
+            printf 'Error: "%s" is not installed or not in PATH\n' "${command_name}" >&2
+            exit 1
+        fi
+        shift
+    done
     return 0
 }
 export -f cxxet_require_command
