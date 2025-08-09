6f0b6979e83ee566360d73820ce0ed35a6c001ee
Lukáš Růžička (asus-haven-w) <ruzovej@gmail.com>
2025-08-09 14:56:23 +0200

    Remove redundant empty line

--- scripts/common/ensure_docker_is_allowed.bash
+++ scripts/common/ensure_docker_is_allowed.bash
@@ -5,7 +5,6 @@ set -e
 function ensure_docker_is_allowed() {
     if [[ -z "${CXXET_AVOID_DOCKER}" ]]; then
         cxxet_require_command docker
-
         return 0
     else
         printf 'Error: Docker is disabled by CXXET_AVOID_DOCKER environment variable\n' >&2
