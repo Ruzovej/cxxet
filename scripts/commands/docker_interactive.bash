4a962052f2fc682c788bac307d322d18ff389321
Lukáš Růžička (asus-haven-w) <ruzovej@gmail.com>
2025-08-09 12:34:06 +0200

    Refactor & polish `docker_interactive`

--- scripts/commands/docker_interactive.bash
+++ scripts/commands/docker_interactive.bash
@@ -1,9 +1,27 @@
 #!/usr/bin/env bash
 
-cxxet_include "scripts/common/docker_run"
+cxxet_include scripts/common/docker_run
+cxxet_include scripts/common/ensure_docker_is_allowed
 
 function docker_interactive() {
-    local image_name_base="${1:-cxxet_compiler}"
+    ensure_docker_is_allowed
+
+    function usage() {
+        printf 'Usage: docker_interactive <image_name_base>\n' >&2
+    }
+
+    if [[ "$1" == "--help" || "$1" == "-h" ]]; then
+        usage
+        return 0
+    fi
+
+    local image_name_base="$1"
+
+    if [[ -z "${image_name_base}" ]]; then
+        printf 'Error: Image name base is required!\n' >&2
+        usage
+        exit 1
+    fi
 
     docker_run \
         --interactive \
