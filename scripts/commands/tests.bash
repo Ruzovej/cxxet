180fe303d87c1b1e04d71be2053a4f946229ba1e
Lukáš Růžička (asus-haven-w) <ruzovej@gmail.com>
2025-08-10 14:51:19 +0200

    Implement `usage` function in `tests.bash`

--- scripts/commands/tests.bash
+++ scripts/commands/tests.bash
@@ -7,28 +7,50 @@ cxxet_include scripts/tests/unit_runner
 cxxet_include scripts/tests/valid_examples_runner
 
 function tests() {
+    function usage() {
+        {
+            if [[ "$1" != '--short' ]]; then
+                printf 'tests: run various test suites\n'
+            fi
+            printf 'Usage: tests [options...]\n'
+            printf 'Where options are:\n'
+            printf '    -a, --all, all            Run all suites (unit, examples, bats)\n'
+            printf '    -u, --unit, unit          Run unit tests\n'
+            printf '    -e, --examples, examples  Run "valid" examples\n'
+            printf '    -b, --bats, bats          Run bats tests\n'
+            printf '    --help, -h                Show this help message\n'
+            printf 'For suite-specific details/help, pass -h|--help to it\n'
+        } >&2
+    }
+
     case "$1" in
-        -a|--all)
+        -a|--all|all)
             shift
             unit_runner "$@"
             valid_examples_runner "$@"
             bats_runner "$@"
             ;;
-        -b|--bats)
+        -b|--bats|bats)
             shift
             bats_runner "$@"
             ;;
-        -e|--examples)
+        -e|--examples|examples)
             shift
             valid_examples_runner "$@"
             ;;
-        -u|--unit)
+        -u|--unit|unit)
             shift
             unit_runner "$@"
+            ;;
+        --help|-h)
+            usage
+            exit 0
             ;;
         *)
             printf 'Unknown option "%s"\n' "$1" >&2
+            usage --short
             exit 1
             ;;
     esac
+    return 0
 }
