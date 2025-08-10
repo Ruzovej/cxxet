4776bdf9fafbba0ac83e1305c48c32a7a39f6cfe
Lukáš Růžička (asus-haven-w) <ruzovej@gmail.com>
2025-08-10 18:46:22 +0200

    Small cleanup in `abi_check` & fix version deduction in `CMakeLists.txt`

--- scripts/commands/abi_check.bash
+++ scripts/commands/abi_check.bash
@@ -9,20 +9,20 @@ function abi_check() {
         abidiff \
         git
 
-    local preset=deploy # TODO later (after merging this into `main` ...) change to `abidiff`
+    local preset=relwithdebinfo # TODO (https://github.com/Ruzovej/cxxet/issues/113) change to `abidiff`
     local prev_commit
     local prev_branch=main
 
     function usage() {
         {
             if [[ "$1" != '--short' ]]; then
-                printf 'abi_check: configure & execute build system for given preset, target(s), etc.'
+                printf 'abi_check: compare abi of previous library build with current one\n'
             fi
             printf 'Usage: abi_check [options...]\n'
             printf 'Where options are:\n'
             printf '    --preset, -p PRESET        Set the CMake preset (default: %s)\n' "${preset}"
-            printf '    --commit, -c COMMIT        Compare with the specified commit, overrides "branch"\n'
-            printf '    --branch, -b BRANCH        Compare with last commit on specified branch (default: %s)\n' "${prev_branch}"
+            printf '    --commit, -c COMMIT        Compare with the specified commit. If not specified, uses last one\n'
+            printf '    --branch, -b BRANCH        Compare with given commit on specified branch (default: %s)\n' "${prev_branch}"
             printf '    --help, -h                 Show this help message\n'
         } >&2
     }
@@ -30,7 +30,7 @@ function abi_check() {
     while (( $# > 0 )); do
         case "$1" in
             --preset|-p)
-                cxxet_preset="${2:?No preset specified!}"
+                preset="${2:?No preset specified!}"
                 shift 2
                 ;;
             --commit|-c)
@@ -63,7 +63,7 @@ function abi_check() {
     local abidiff_dir="${CXXET_ROOT_DIR}/tmp/cloned_repo_for_abidiff/preset_${preset}"
     local baseline_so_name="${abidiff_dir}/bin/${preset}/libcxxet.so"
     (
-        #rm -rf "${abidiff_dir}" # TODO (#56) remove later - now it's here for testing purposes ...
+        #rm -rf "${abidiff_dir}" # TODO (https://github.com/Ruzovej/cxxet/issues/113) remove later - now it's here for testing purposes ...
         if [[ ! -d "${abidiff_dir}" ]]; then
             mkdir -p "${abidiff_dir}"
             (
@@ -86,6 +86,7 @@ function abi_check() {
         (
             set -x
             git -C "${abidiff_dir}" checkout \
+                --quiet \
                 --detach \
                 "${prev_commit}"
         )
@@ -101,6 +102,7 @@ function abi_check() {
         }
     ) >&2
 
+    # TODO (https://github.com/Ruzovej/cxxet/issues/113) implement `--quiet` option for `compile` subcommand & use it (at least) here
     local current_so_name="${CXXET_ROOT_DIR}/bin/${preset}/libcxxet.so"
     compile \
         --preset "${preset}" \
@@ -114,7 +116,8 @@ function abi_check() {
     (
         set -x
         abidiff \
-            --ignore-soname \
+            --harmless \
+            --redundant \
             "${baseline_so_name}" \
             "${current_so_name}"
     ) >&2
