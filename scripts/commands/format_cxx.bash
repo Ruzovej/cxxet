f09ce12a2a32761ead091afb24db7954555a5975
Lukáš Růžička (asus-haven-w) <ruzovej@gmail.com>
2025-08-10 13:06:32 +0200

    Implement `usage` function in `format_cxx.bash`

--- scripts/commands/format_cxx.bash
+++ scripts/commands/format_cxx.bash
@@ -2,11 +2,39 @@
 
 set -e
 
+cxxet_include scripts/common/reject_further_args
+
 function format_cxx() {
     cxxet_require_command clang-format
 
+    local format_folders=(
+        "${CXXET_ROOT_DIR}/examples"
+        "${CXXET_ROOT_DIR}/include"
+        "${CXXET_ROOT_DIR}/src"
+        "${CXXET_ROOT_DIR}/tests"
+    )
+
+    function usage() {
+        if [[ "$1" != '--short' ]]; then
+            printf 'format_cxx: formats all C/C++ source files in %s\n' >&2 "${format_folders[*]}"
+        fi
+        printf 'Usage: format_cxx [options...]\n' >&2
+        printf 'Where options are:\n' >&2
+        printf '    --help, -h    Show this help message\n' >&2
+    }
+
+    if [[ "$1" == "--help" || "$1" == "-h" ]]; then
+        usage
+        return 0
+    fi
+
+    reject_further_args "$@" || {
+        usage --short
+        exit 1
+    }
+
     find \
-        "${CXXET_ROOT_DIR}/examples" "${CXXET_ROOT_DIR}/include" "${CXXET_ROOT_DIR}/src" "${CXXET_ROOT_DIR}/tests" \
+        "${format_folders[@]}" \
         \( -name "*.cxx" -o -name "*.hxx" -o -name "*.c" -o -name "*.h" \) \
         -type f \
         -exec \
