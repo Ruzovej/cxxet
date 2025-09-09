b1fc2c94ba8a2cab76168b75728d9e40edfd0d76
Lukáš Růžička (asus-haven-w) <ruzovej@gmail.com>
2025-09-09 20:43:43 +0000

    WIP: add basic frame for "large" benchmarks

---
+++ examples/for_benchmarks/bench_init.cxx
@@ -0,0 +1,28 @@
+/*
+  Copyright 2025 Lukáš Růžička
+
+  This file is part of cxxet.
+
+  cxxet is free software: you can redistribute it and/or modify it under the
+  terms of the GNU Lesser General Public License as published by the Free
+  Software Foundation, either version 3 of the License, or (at your option) any
+  later version.
+
+  cxxet is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+  A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+  details.
+
+  You should have received a copy of the GNU Lesser General Public License along
+  with cxxet. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "cxxet/basic.hxx"
+
+int main(int const, char const **) {
+  CXXET_sink_thread_reserve();
+
+  CXXET_sink_thread_flush_now();
+
+  return 0;
+}
