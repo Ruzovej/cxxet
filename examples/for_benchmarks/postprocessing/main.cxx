31c9675c3407a65cc907beda4507af15ce401b52
Lukáš Růžička (asus-haven-w) <ruzovej@gmail.com>
2025-09-28 20:45:32 +0000

    WIP `cxxet_large_bench_postprocess`

--- examples/for_benchmarks/postprocessing/main.cxx
+++ examples/for_benchmarks/postprocessing/main.cxx
@@ -19,4 +19,92 @@
 
 #include <cstdlib>
 
-int main(int const /*argc*/, char const ** /*argv*/) { return EXIT_SUCCESS; }
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <string_view>
+
+#include "../../../include/public/cxxet/timepoint.hxx"
+#include <nlohmann/json.hpp>
+#include <utility>
+
+namespace {
+
+long long now() noexcept { return cxxet::impl::as_int_ns(cxxet::impl::now()); }
+
+void log_time_diff(std::string_view const msg, long long const begin,
+                   long long const end) {
+  std::cout << msg << ": " << static_cast<double>(end - begin) / 1'000'000
+            << " [ms]\n";
+}
+
+bool ends_with(std::string_view str, std::string_view suffix) {
+  return str.size() >= suffix.size() &&
+         str.compare(str.size() - suffix.size(), suffix.size(), suffix) == 0;
+}
+
+nlohmann::json process_benchmark(std::filesystem::path const &meta_file_path) {
+  return meta_file_path.string();
+}
+
+} // namespace
+
+int main(int const argc, char const **argv) try {
+  std::filesystem::path results_dir;
+  if (argc > 1) {
+    results_dir = argv[1];
+  } else {
+    throw "missing input directory argument";
+  }
+  if (!std::filesystem::is_directory(results_dir)) {
+    throw "input path '" + results_dir.string() + "' is not a directory";
+  }
+
+  auto t0{now()};
+
+  nlohmann::json meta_info = {
+      {"cxxet_git_hash",
+       nlohmann::json::parse(std::ifstream{
+           results_dir / "commit_hash.json"})["context"]["cxxet_git_hash"]
+           .get<std::string>()},
+  };
+
+  nlohmann::json benchmarks = nlohmann::json::array();
+
+  for (auto const &entry : std::filesystem::directory_iterator{results_dir}) {
+    if (!entry.is_regular_file()) {
+      continue;
+    }
+
+    static constexpr std::string_view meta_file_suffix{"_meta.json"};
+    auto const entry_path{entry.path()};
+    if (ends_with(entry_path.string(), meta_file_suffix)) {
+      benchmarks.push_back(process_benchmark(entry_path));
+    }
+  }
+
+  auto t1{now()};
+  log_time_diff("Postprocessed benchmark results", t0, t1);
+
+  nlohmann::json result = {
+      {"context", std::move(meta_info)},
+      {"benchmarks", std::move(benchmarks)},
+  };
+
+  std::ofstream ofs{results_dir / "large.json"};
+  ofs << result.dump(2);
+
+  log_time_diff("Saved results into file", t1, now());
+
+  return EXIT_SUCCESS;
+} catch (std::string const &msg) {
+  std::cerr << "Failed to postprocess \"large\" benchmark results: " << msg
+            << std::endl;
+  return EXIT_FAILURE;
+
+} catch (char const *const msg) {
+  std::cerr << "Failed to postprocess \"large\" benchmark results: " << msg
+            << std::endl;
+  return EXIT_FAILURE;
+}
